ebe9277beadae2f9807fddd4540829397192b1c5
antonioojosep <antonillocarriillo@gmail.com>
2024-12-03 18:40:55 +0100

    proyecto terminado

--- Proyects/Foro/models/ResponseRepository.php
+++ Proyects/Foro/models/ResponseRepository.php
@@ -1,7 +1,21 @@
 <?php
 class ResponseRepository
 {
-    // public static function getTwoLastResponseOfMatter($id) {
+    public static function addResponse($text,$matter,$user)
+    {
+        $db = Connection::connect();
+        $q = "INSERT INTO response (`text`,`matter`,`user`) VALUES ('$text','$matter','$user')";
+        $db->query($q);
+    }
 
-    // }
+    public static function statusHandler($id,$status)
+        {
+            $db = Connection::connect();
+            if ($status == 'inactive') {
+                $q = "UPDATE response set status='active' WHERE id='$id'";
+            }else {
+                $q = "UPDATE response set status='inactive' WHERE id='$id'";
+            }
+            $db->query($q);
+            }
 }
